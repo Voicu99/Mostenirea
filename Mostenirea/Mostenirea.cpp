430aa6b74773346f56e594643966a75d1bc1d3a2
Voicu Librimir <voicu.librimir@gmail.com>
2024-04-23 20:07:17 +0300

    Add project files.

---
+++ Mostenirea/Mostenirea.cpp
@@ -0,0 +1,34 @@
+// Mostenirea.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+class Vehicul
+{
+public:
+    std::string marca;
+    void porneste()
+    {
+        std::cout << "Vehiculul a pornit " << std::endl;
+    }
+};
+
+//class Derivata: tip_mostenire clasa_de_baza
+class Automobil : public Vehicul
+{
+public:
+    void claxoneaza()
+    {
+        std::cout << "Beep Beep! " << std::endl;
+    }
+};
+
+
+int main()
+{
+    Automobil bmw;
+    bmw.marca = "Bmw";//atribut din clasa vehicul
+    bmw.porneste();//metoda din clasa vehicul
+    bmw.claxoneaza();//metoda din clasa automobil
+  
+}
